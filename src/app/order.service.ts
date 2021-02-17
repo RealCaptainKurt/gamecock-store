9a2122f69ec38072b542ba3ecadad20d937b23af
RealCaptainKurt <66217173+RealCaptainKurt@users.noreply.github.com>
2021-02-16 21:22:10 -0500

    Making a backup before making big changes

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -5,9 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class OrderService {
 
-  orders=[
-    {id:1,name:"Cocky Plushie",photo:'https://i.pinimg.com/originals/df/cc/e6/dfcce6db471775803f8a37aa7d9876a9.jpg',price:10,quantity:2,date:"2021-2-16",amount:20},
-    {id:2,name:"Hot Pockets",photo:'https://www.goodnes.com/sites/g/files/jgfbjl131/files/gdn_product/field_product_images/hotpockets-fgrhlkasilhqerh6b7fj.png',price:4,quantity:4,date:"2021-2-16",amount:16}
+  orders=[{id:1.0,name:"Cocky Plushie",photo:'https://i.pinimg.com/originals/df/cc/e6/dfcce6db471775803f8a37aa7d9876a9.jpg',price:10,quantity:2,date:"2021-2-16",amount:20},
+    {id:2.0,name:"Hot Pockets",photo:'https://www.goodnes.com/sites/g/files/jgfbjl131/files/gdn_product/field_product_images/hotpockets-fgrhlkasilhqerh6b7fj.png',price:4,quantity:4,date:"2021-2-16",amount:16}
   ]
 
   lastID=2
